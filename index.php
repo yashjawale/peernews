5f046019a9432ad51e5cfa6978acac49c8ec4c82
yashjawale <yash25.j@gmail.com>
2024-06-02 17:42:56 +0530

    Add homepage content

--- index.php
+++ index.php
@@ -1,5 +1,51 @@
 <?php require("components/pagestart.php") ?>
 
-<h1>Welcome to PeerNews</h1>
+<main class="container mx-auto px-6 py-20">
+  <h1 class="text-4xl font-bold">Latest Updates</h1>
+  <div class="grid grid-cols-3 py-8 gap-4">
+
+    <div class="border-[1px] border-black rounded-xl overflow-hidden">
+      <img src="https://picsum.photos/500/400" alt="Placeholder" class="w-full h-48 object-cover object-center">
+      <div class="p-4 flex flex-col gap-2">
+        <p class="text-neutral-500 uppercase text-sm font-light">02 JUNE 2024 8:24 PM</p>
+        <h2 class="font-semibold text-2xl">Highest temperature in Pune City since last two years.</h2>
+        <p class="text-neutral-500 font-light">By Ramesh Hilton</p>
+      </div>
+    </div>
+    <div class="border-[1px] border-black rounded-xl overflow-hidden">
+      <img src="https://picsum.photos/500/400" alt="Placeholder" class="w-full h-48 object-cover object-center">
+      <div class="p-4 flex flex-col gap-2">
+        <p class="text-neutral-500 uppercase text-sm font-light">02 JUNE 2024 8:24 PM</p>
+        <h2 class="font-semibold text-2xl">Highest temperature in Pune City since last two years.</h2>
+        <p class="text-neutral-500 font-light">By Ramesh Hilton</p>
+      </div>
+    </div>
+    <div class="border-[1px] border-black rounded-xl overflow-hidden">
+      <img src="https://picsum.photos/500/400" alt="Placeholder" class="w-full h-48 object-cover object-center">
+      <div class="p-4 flex flex-col gap-2">
+        <p class="text-neutral-500 uppercase text-sm font-light">02 JUNE 2024 8:24 PM</p>
+        <h2 class="font-semibold text-2xl">Highest temperature in Pune City since last two years.</h2>
+        <p class="text-neutral-500 font-light">By Ramesh Hilton</p>
+      </div>
+    </div>
+    <div class="border-[1px] border-black rounded-xl overflow-hidden">
+      <img src="https://picsum.photos/500/400" alt="Placeholder" class="w-full h-48 object-cover object-center">
+      <div class="p-4 flex flex-col gap-2">
+        <p class="text-neutral-500 uppercase text-sm font-light">02 JUNE 2024 8:24 PM</p>
+        <h2 class="font-semibold text-2xl">Highest temperature in Pune City since last two years.</h2>
+        <p class="text-neutral-500 font-light">By Ramesh Hilton</p>
+      </div>
+    </div>
+    <div class="border-[1px] border-black rounded-xl overflow-hidden">
+      <img src="https://picsum.photos/500/400" alt="Placeholder" class="w-full h-48 object-cover object-center">
+      <div class="p-4 flex flex-col gap-2">
+        <p class="text-neutral-500 uppercase text-sm font-light">02 JUNE 2024 8:24 PM</p>
+        <h2 class="font-semibold text-2xl">Highest temperature in Pune City since last two years.</h2>
+        <p class="text-neutral-500 font-light">By Ramesh Hilton</p>
+      </div>
+    </div>
+
+  </div>
+</main>
 
 <?php require("components/pageend.php") ?>
