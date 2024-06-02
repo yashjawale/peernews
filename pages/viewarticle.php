35ccde161052a61a4b3b404ab207275ae7b830af
yashjawale <yash25.j@gmail.com>
2024-06-02 18:09:21 +0530

    Add article page

---
+++ pages/viewarticle.php
@@ -0,0 +1,16 @@
+<?php include("../components/pagestart.php") ?>
+
+<main class="container mx-auto px-6 py-20">
+  <div class="md:w-11/12 lg:w-8/12">
+    <h1 class="text-4xl font-bold">Pune records highest temperatures since last two years.</h1>
+    <div class="py-8">
+      <p class="text-neutral-500 uppercase text-sm font-light">02 JUNE 2024 8:24 PM</p>
+      <img src="https://picsum.photos/800/600" alt="Placeholder" class="w-full h-[400px] mt-8 rounded-xl object-cover object-center">
+      <p class="py-8">Pune, the cultural capital of Maharashtra, has recorded the highest temperatures since the last two years. The temperature has been recorded at 45 degrees Celsius. The city has been facing a heatwave for the past few days. The citizens are advised to stay indoors and keep themselves hydrated.</p>
+      <p class="text-neutral-500 font-light">By Ramesh Hilton</p>
+
+    </div>
+  </div>
+</main>
+
+<?php include("../components/pageend.php") ?>
