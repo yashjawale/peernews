163b440cab098e113b122c24e0ffc5642d213abc
yashjawale <yash25.j@gmail.com>
2024-06-02 18:30:49 +0530

    Make header link point to index

--- components/header.php
+++ components/header.php
@@ -1,7 +1,7 @@
 <header class="bg-primary py-6 text-white sticky top-0">
   <nav class="container mx-auto px-6">
     <div class="flex justify-between">
-      <p class="text-2xl font-bold">PeerNews</p>
+      <a href="../index.php" class="text-2xl font-bold">PeerNews</a>
       <div class="flex gap-3 items-center">
         <p class="hidden md:block">Create an account to add your article.</p>
         <a href="../pages/register.php" class="bg-white text-primary text-sm px-4 py-2 rounded-lg hover:opacity-70">Sign Up</a>
